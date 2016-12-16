ce1004633677dd039a6277395238dad5d4fa47f8
carmenluyo <c.luyorosales@gmail.com>
2016-12-16 00:01:10 -0500

    demos grid

---
+++ grid2/assets/js/custom.js
@@ -0,0 +1,39 @@
+﻿
+/*=============================================================
+    Authour URL: www.designbootstrap.com
+
+    http://www.designbootstrap.com/
+
+    License: MIT
+
+    http://opensource.org/licenses/MIT
+
+    100% Free To use For Personal And Commercial Use.
+
+    IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
+   
+========================================================  */
+
+$(document).ready(function () {
+
+    // SCROLL SCRIPTS 
+    $('.scroll-me a').bind('click', function (event) { //just pass scroll-me class and start scrolling
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutQuad');
+        event.preventDefault();
+    });
+
+    //FANCY BOX PLUGIN SCRIPT FOR POPUP IMAGE
+    $('.fancybox-media').fancybox({
+        openEffect: 'elastic',
+        closeEffect: 'elastic',
+        helpers: {
+            title: {
+                type: 'inside'
+            }
+        }
+    });
+
+});
